a5fa7d0011ffc46329efd97d235cde37045f09aa
Carlos R. Rendon <carlos.ramirezrn@outlook.com>
2022-05-17 16:04:10 -0500

    Changes to invoice function

--- billing/src/main/java/com/udemy/billing/controller/BillingController.java
+++ billing/src/main/java/com/udemy/billing/controller/BillingController.java
@@ -5,10 +5,7 @@ import com.udemy.billing.repository.BillingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +22,7 @@ public class BillingController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Invoice> save(Invoice invoice){
+    public ResponseEntity<Invoice> save(@RequestBody Invoice invoice){
         billingRepository.save(invoice);
         return new ResponseEntity<>(invoice,HttpStatus.CREATED);
     }
